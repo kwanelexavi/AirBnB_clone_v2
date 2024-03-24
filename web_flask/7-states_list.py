c3aa845330aa9098389bd9afcc4489bba10dcd22
kwanelexavi <kwanelexavi@gmail.com>
2024-03-24 15:51:25 -0700

    8 update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""
-    Script that starts a Flask Web application listening on
-    0.0.0.0:5000
-"""
-
+""" Starts a Flash Web Application """
 from models import storage
+from models.state import State
 from flask import Flask, render_template
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """
-        displays a html page containing states
-    """
-    return render_template('7-states_list.html', states=storage.all("State"))
+    """ displays a HTML page with a list of states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown(err):
-    storage.close()
-
-
-if __name__ == '__main__':
-    """
-        make web page accessible
-    """
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
